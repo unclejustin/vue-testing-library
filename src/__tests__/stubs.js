6d072fbd2822bf1d63ae45f225f9d0d18b17b35d
jboyson1 <jboyson@gitlab.com>
2020-01-21 16:52:12 -0600

    Add test for shallowMount

--- src/__tests__/stubs.js
+++ src/__tests__/stubs.js
@@ -1,9 +1,16 @@
-import {render} from '@testing-library/vue'
+import { render, shallowMount } from '@testing-library/vue'
 import Stubs from './components/Stubs'
 
 test('Form contains search button', () => {
-  const {getByText} = render(Stubs, {
+  const { getByText } = render(Stubs, {
     stubs: ['FontAwesomeIcon'],
   })
   getByText('Search now')
+})
+
+test('Form does not contain search button', () => {
+  const { queryByText } = shallowMount(Stubs, {
+    stubs: ['FontAwesomeIcon'],
+  })
+  expect(queryByText('Search now')).toBeNull()
 })
